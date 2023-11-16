64fa58fc8c225d753bdd6e9471b6433055e7bde8
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2023-11-17 04:57:19 +0530

    QuestionI solved

--- QuestionI/QuestionI.cpp
+++ QuestionI/QuestionI.cpp
@@ -1,26 +1,74 @@
 #include <iostream>
 #include <vector>
-#include <pair>
+#include <utility>
+#include <algorithm>
+#include <queue>
+
 using namespace std;
 
+int primAlgo(vector<pair<int, int>> graph[], int n)
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    bool visited[n] = {false};
+    pq.push({0, 0});
+    int sum = 0;
+
+    while (!pq.empty())
+    {
+        auto it = pq.top();
+        pq.pop();
+
+        int node = it.second;
+        int wt = it.first;
+
+        if (visited[node])
+        {
+            continue;
+        }
+
+        // add to the MST
+        visited[node] = true;
+        sum += wt;
+
+        for (auto neighbour : graph[node])
+        {
+            // pushing neighbours to the priority queue
+            int neighbour_vertice = neighbour.second;
+            int neighbour_wt = neighbour.first;
+
+            if (!visited[neighbour_vertice])
+            {
+                pq.push({neighbour_wt, neighbour_vertice});
+            }
+        }
+    }
+
+    return sum;
+}
+
 int main()
-{ // taking input
+{
+    // taking input
     int n, m;
     cin >> n >> m;
-    vector<pair<int, int>> graph[n] for (int i = 0; i < m; i++)
+    vector<pair<int, int>> graph[n];
+
+    for (int i = 0; i < m; i++)
     {
         int v, u, w;
-        pair<int, int> edge1;
-        pair<int, int> edge2;
+        pair<int, int> edge1, edge2;
 
         cin >> v >> u >> w;
-        edge1.first = v - 1;
-        edge1.secod = w;
-        edge2.first = u - 1;
-        edge2.second = w;
+        edge1.first = w;
+        edge1.second = v - 1;
+        edge2.first = w;
+        edge2.second = u - 1;
         graph[v - 1].push_back(edge2);
         graph[u - 1].push_back(edge1);
     }
+
+    // giving output
+    cout << primAlgo(graph, n) << endl;
 
     return 0;
 }
--- QuestionJ/QuestionJ.cpp
+++ QuestionJ/QuestionJ.cpp
@@ -5,3 +5,29 @@ int main()
 
     return 0;
 }
+#include <iostream>
+#include <vector>
+#include <pair>
+using namespace std;
+
+int main()
+{ // taking input
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> graph[n] for (int i = 0; i < m; i++)
+    {
+        int v, u, w;
+        pair<int, int> edge1;
+        pair<int, int> edge2;
+
+        cin >> v >> u >> w;
+        edge1.first = v - 1;
+        edge1.secod = w;
+        edge2.first = u - 1;
+        edge2.second = w;
+        graph[v - 1].push_back(edge2);
+        graph[u - 1].push_back(edge1);
+    }
+
+    return 0;
+}
